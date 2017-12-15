c3b185ff756cced86f1b98e7ac522040058b6541
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2017-12-15 23:11:23 +0800

    Check vim dep & giveup auto color-coded install

--- install.py
+++ install.py
@@ -3,6 +3,7 @@
 from distutils.util import strtobool
 from distutils.spawn import find_executable
 from os import system, abort
+from subprocess import check_output
 
 print "----------------------------------------"
 print "|                                      |"
@@ -16,8 +17,8 @@ print "\033[31mThis installer will override your current settings!\033[m"
 print "\033[33mIt may take a long time to install.\033[m"
 
 # Check dependency
-if not (find_executable('wget') and find_executable('git')):
-    print "\033[31mThis installer depends on git and wget.\033[m"
+if not (find_executable('wget') and find_executable('git') and find_executable('make') and find_executable('cmake')):
+    print "\033[31mThis installer depends on git, wget, build-essential, and cmake.\033[m"
     abort()
 
 # Check zsh install
@@ -25,8 +26,17 @@ if not find_executable('zsh'):
     print "\033[31mInstall zsh before running this script.\033[m"
     abort()
 
+# Check vim
+if not find_executable('vim') or '+python' not in check_output(['vim', '--version']):
+    print "\033[31mThe configuration requires vim with python support.\033[m"
+    abort()
+
 
 if strtobool(raw_input('Do you want to install with color_coded? (y/n)')):
+    if '+lua' not in check_output(['vim', '--version']):
+        print "\033[31mColor_coded requires vim with lua supported"
+        abort()
+
     branch = 'with-color_coded'
 else:
     branch = 'master'
@@ -62,8 +72,8 @@ run_shell('cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer')
 run_shell('wget https://raw.githubusercontent.com/JDevlieghere/dotfiles/master/.vim/.ycm_extra_conf.py -O ~/.vim/.ycm_extra_conf.py')
 
 if branch == 'with-color_coded':
-    print "\n---- [ 7. ] Install color_coded for vim"
-    run_shell('cd ~/.vim/bundle/color_coded && mkdir build && cd build && cmake .. && make && make install && make clean && make clean_clang')
+    print "\n\033[33mThe color_coded installation is quite complicated,"
+    print "\033[33mplease refer https://github.com/jeaye/color_coded#ubuntudebian\033[m"
 
 print "\n---- Installation Completed! ----"
 print "It's time to change your shell... (password may be required)"
